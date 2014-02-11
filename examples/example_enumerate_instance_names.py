136054c856464b9db91bbf476e66fb42a22bb3d5
Peter Hatina <phatina@redhat.com>
2014-02-11 10:19:11 +0100

    fix EnumerateInstanceNames() example

--- examples/example_enumerate_instance_names.py
+++ examples/example_enumerate_instance_names.py
@@ -10,23 +10,25 @@
 #   hostname
 #   username
 #   password
+#   cls
 
 import lmiwbem
 
 hostname = "hostname"
 username = "username"
 password = "password"
+cls      = "class"
 
 # Connect to CIMOM
 conn = lmiwbem.WBEMConnection()
 conn.connect(hostname, username, password)
 
-# Enumerate Instances
-classes = conn.EnumerateInstanceNames(
+# Enumerate instance names
+inames = conn.EnumerateInstanceNames(
     cls, "root/cimv2")
 
-# Do something with instances
-print account_names
+# Do something with instance names
+print inames
 
 # Disconnect from CIMOM
 conn.disconnect()
