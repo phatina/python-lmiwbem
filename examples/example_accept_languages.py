2ddc52cb7ef426c44f8c4c332b8ebd203d68752c
Alexander Lakhin <a.lahin@ntcit-rosa.ru>
2014-09-23 13:33:45 +0200

    introduce WBEMConnection::request_accept_languages

---
+++ examples/example_accept_languages.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+#
+# Author: alexanderlaw <a.lahin@ntcit-rosa.ru>
+#
+# This example demonstrates how to get/set the accept languages.
+
+import lmiwbem
+
+conn = lmiwbem.WBEMConnection()
+
+# Get Accept languages
+print conn.request_accept_languages
+
+# Set Accept languages
+conn.request_accept_languages = [("ru", 1.0), ("en", 0.5)]
